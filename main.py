f798935b2e2d107f36071146841f2631e7f93e62
Nicholas Mendez <snickdx@gmail.com>
2020-05-14 16:58:49 -0400

    added boilerplate code for both auth methods

--- main.py
+++ main.py
@@ -2,17 +2,30 @@ import json
 from flask_cors import CORS
 from flask_login import LoginManager, current_user, login_user, login_required
 from flask import Flask, request, render_template, redirect, flash, url_for
+from flask_jwt import JWT, jwt_required, current_identity
 from sqlalchemy.exc import IntegrityError
+from datetime import timedelta 
 
-from models import db, Logs
+from models import db, Logs #add application models
 
 ''' Begin boilerplate code '''
+
+''' Begin Flask Login Functions '''
+# login_manager = LoginManager()
+# @login_manager.user_loader
+# def load_user(user_id):
+#     return User.query.get(user_id)
+
+''' End Flask Login Functions '''
+
 def create_app():
   app = Flask(__name__, static_url_path='')
   app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
   app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
   app.config['SECRET_KEY'] = "MYSECRET"
+#   app.config['JWT_EXPIRATION_DELTA'] = timedelta(days = 7) # uncomment if using flsk jwt
   CORS(app)
+#   login_manager.init_app(app) # uncomment if using flask login
   db.init_app(app)
   return app
 
@@ -21,6 +34,17 @@ app = create_app()
 app.app_context().push()
 
 ''' End Boilerplate Code '''
+
+''' Set up JWT here (if using flask JWT)'''
+# def authenticate(uname, password):
+#   pass
+
+# #Payload is a dictionary which is passed to the function by Flask JWT
+# def identity(payload):
+#   pass
+
+# jwt = JWT(app, authenticate, identity)
+''' End JWT Setup '''
 
 @app.route('/')
 def index():
